b2645148ed9c5a34f5f8696456e05ef452dc65f6
Kate Fanina <katherina.fanina@gmail.com>
2022-12-12 15:10:20 +0200

    add gallery modal pannnel and carousel

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+let selectedImage;
+const body = document.body;
+const gallery = document.querySelector('.gallery');
+const galleryHtmlText = galleryItems
+  .map(
+    ({ preview, original, description }) => `
+    <div class="gallery__item">
+        <a class="gallery__link" href=${original}>
+        <img class="gallery__image" src=${preview} data-source=${original} alt=${description} />
+       </a>
+    </div>`
+  )
+  .join('');
+gallery.insertAdjacentHTML('beforeend', galleryHtmlText);
+
+gallery.addEventListener('click', openImage);
+body.addEventListener('keydown', closeImage);
+
+function openImage(event) {
+  event.preventDefault();
+  const target = event.target;
+  if (target.nodeName !== 'IMG') {
+    return;
+  }
+  selectedImage = basicLightbox.create(
+    `<img src=${target.dataset.source} alt=${target.alt} width="800" height="600" />`
+  );
+  selectedImage.show();
+}
+
+function closeImage(event) {
+  if (event.code === 'Escape') {
+    selectedImage.close();
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const galleryHtmlText = galleryItems
+  .map(
+    ({ preview, original, description }) => ` 
+       <a data-type="image" class="gallery__item" href=${original}>
+          <img alt=${description} class="gallery__image" src=${preview} />
+       </a>`
+  )
+  .join('');
+gallery.insertAdjacentHTML('beforeend', galleryHtmlText);
+
+gallery.addEventListener('click', openImage);
+
+function openImage(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    scrollZoom: false,
+  });
+}
